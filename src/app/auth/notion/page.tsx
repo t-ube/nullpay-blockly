705814de5c1695b14840921686adcf3933000eef
t-ube <ube@coder.okinawa.jp>
2024-06-27 00:20:07 +0900

    #25-test

--- src/app/auth/notion/page.tsx
+++ src/app/auth/notion/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 export default function SignIn() {
-  const notionAuthUrl = `https://api.notion.com/v1/oauth/authorize?owner=user&client_id=${process.env.NEXT_PUBLIC_NOTION_OAUTH_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NOTION_REDIRECT_URI}&response_type=code`;
+  const notionAuthUrl = `https://api.notion.com/v1/oauth/authorize?owner=user&client_id=${process.env.NEXT_PUBLIC_NOTION_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NOTION_REDIRECT_URI}&response_type=code`;
 
   return (
     <div>
