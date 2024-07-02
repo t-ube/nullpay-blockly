f0fc1aa35042c648619d31168c3158e568225bc3
t-ube <ube@coder.okinawa.jp>
2024-07-02 21:13:19 +0900

    #25-test

--- src/app/auth/notion/callback/page.tsx
+++ src/app/auth/notion/callback/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState, Suspense } from 'react';
-import xamanPkce from '@/utils/XamanPkce';
+import { useEffect, Suspense } from 'react';
 
 const NotionCallback = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
   const error = searchParams.get('error');
-  const [accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
     if (error) {
@@ -26,22 +24,6 @@ const NotionCallback = () => {
           console.error('Token exchange error:', data.error);
         } else {
           console.log('Token exchange success:', data);
-          setAccessToken(data.access_token);
-
-          const fetchData = async () => {
-            try {
-              const state = await xamanPkce.state();
-              if (state?.me) {
-                const { sdk } = state;
-                await sdk.jwtUserdata.set('notion', data.access_token);
-              } else {
-                throw new Error('Not logged in');
-              }
-            } catch (error) {
-              console.error('Failed to save notion token:', error);
-            }
-          };
-          fetchData();
         }
       })
       .catch(console.error);
@@ -52,7 +34,6 @@ const NotionCallback = () => {
     <>
       <h1>Notion OAuth Callback</h1>
       {error ? <p>Authentication failed: {error}</p> : <p>Processing authentication...</p>}
-      {accessToken && {accessToken}}
     </>
   );
 };
--- src/app/auth/notion/page.tsx
+++ src/app/auth/notion/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import React, { useState } from 'react';
-import SignInModal from '@/components/NotionSignInModal';
+import NotionSignInModal from '@/components/NotionSignInModal';
 import { Button } from '@mui/material';
+import xamanPkce from '@/utils/XamanPkce';
 
 export default function SignIn() {
   //const notionAuthUrl = `https://api.notion.com/v1/oauth/authorize?owner=user&client_id=${process.env.NEXT_PUBLIC_NOTION_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NOTION_REDIRECT_URI}&response_type=code`;
@@ -20,7 +21,20 @@ export default function SignIn() {
   const handleTokenReceived = (token: string) => {
     setToken(token);
     console.log('Token received:', token);
-    // ここでトークンを処理するロジックを追加できます
+    const fetchData = async () => {
+      try {
+        const state = await xamanPkce.state();
+        if (state?.me) {
+          const { sdk } = state;
+          await sdk.jwtUserdata.set('notion', [token]);
+        } else {
+          throw new Error('Not logged in');
+        }
+      } catch (error) {
+        console.error('Failed to save notion token:', error);
+      }
+    };
+    fetchData();
   };
   
   return (
@@ -32,7 +46,7 @@ export default function SignIn() {
         <Button variant="contained" color="primary" onClick={handleOpen}>
           Sign In with Notion
         </Button>
-        <SignInModal open={open} onClose={handleClose} onTokenReceived={handleTokenReceived} />
+        <NotionSignInModal open={open} onClose={handleClose} onTokenReceived={handleTokenReceived} />
         {token && <p>Received token: {token}</p>}
       </div>
     </div>
